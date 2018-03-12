6e53261b9da9f627889042fc0543511291868bd9
unknown <Tech@C401IT22.gbc.local>
2018-03-12 08:57:42 -0400

    2nd commit
    merging

--- app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
+++ app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
@@ -1,12 +1,39 @@
 package com.example.tahasaleem.midterm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ImageView;
+import android.media.Image;
 
-public class MainActivity extends AppCompatActivity {
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
+    // Int variables for card game
+    final int numCards = 52;
+    private int playerScore = 0;
+
+    // Image array for cards
+    Image cards[] = new Image[numCards];
+    String fileLocation = new String();
+
+    // Alerts
+    AlertDialog.Builder playAgain;
+
+    private TextView scoreText;
+    private TextView countDown;
+    private TextView pickCardText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
