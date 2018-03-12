47a8b87a57aeb01f9ccf5c387f40a81f9385d346
unknown <Tech@C401IT22.gbc.local>
2018-03-12 09:43:05 -0400

    3rd comit
    code

--- app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
+++ app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tahasaleem.midterm;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.view.View;
 import android.view.Window;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.widget.ImageView;
 import android.media.Image;
+import android.widget.ImageButton;
+
 
 
 import org.w3c.dom.Text;
@@ -21,9 +24,10 @@ public class MainActivity extends AppCompatActivity
     private int playerScore = 0;
 
     // Image array for cards
-    Image cards[] = new Image[numCards];
-    String fileLocation = new String();
-
+    int cards[] = new int[] {R.drawable.card_1c, R.drawable.card_1d, R.drawable.card_1h, R.drawable.card_1s, R.drawable.card_2c, R.drawable.card_2d, R.drawable.card_2h, R.drawable.card_2s,
+                                            R.drawable.card_3c, R.drawable.card_3d, R.drawable.card_3h, R.drawable.card_3s, R.drawable.card_4c,R.drawable.card_4d,R.drawable.card_4h,R.drawable.card_4s,
+                                            R.drawable.card_5c, R.drawable.card_5d, R.drawable.card_5h, R.drawable.card_5s, R.drawable.card_6c, R.drawable.card_6d, R.drawable.card_6h, R.drawable.card_6s,
+                                            R.drawable.card_7c, R.drawable.card_7d, R.drawable.card_7h, R.drawable.card_7s};
     // Alerts
     AlertDialog.Builder playAgain;
 
@@ -36,5 +40,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void loopCards()
+    {
+        for (int i = 0; i < numCards ; i++)
+        {
+            cards[i] = 
+        }
     }
 }
