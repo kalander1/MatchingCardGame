43777529965948a2e02ae2d9bf798b8d50a567e0
unknown <Tech@C401IT22.gbc.local>
2018-03-12 09:49:29 -0400

    4th
    card drawavles

--- app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
+++ app/src/main/java/com/example/tahasaleem/midterm/MainActivity.java
@@ -3,31 +3,16 @@ package com.example.tahasaleem.midterm;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.widget.ImageButton;
-=======
-<<<<<<< HEAD
+
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
-
-=======
->>>>>>> 721e4caf62488eb22f602ba379b5a7068f153236
 import android.widget.TextView;
-import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.Toast;
 import android.widget.ImageView;
 import android.media.Image;
-<<<<<<< HEAD
-import android.widget.ImageButton;
-
-=======
->>>>>>> 4172f59409b6036f4c8f8d8ed3ea23c28816eed9
->>>>>>> 721e4caf62488eb22f602ba379b5a7068f153236
-
 
 import org.w3c.dom.Text;
 
@@ -41,7 +26,11 @@ public class MainActivity extends AppCompatActivity
     int cards[] = new int[] {R.drawable.card_1c, R.drawable.card_1d, R.drawable.card_1h, R.drawable.card_1s, R.drawable.card_2c, R.drawable.card_2d, R.drawable.card_2h, R.drawable.card_2s,
                                             R.drawable.card_3c, R.drawable.card_3d, R.drawable.card_3h, R.drawable.card_3s, R.drawable.card_4c,R.drawable.card_4d,R.drawable.card_4h,R.drawable.card_4s,
                                             R.drawable.card_5c, R.drawable.card_5d, R.drawable.card_5h, R.drawable.card_5s, R.drawable.card_6c, R.drawable.card_6d, R.drawable.card_6h, R.drawable.card_6s,
-                                            R.drawable.card_7c, R.drawable.card_7d, R.drawable.card_7h, R.drawable.card_7s};
+                                            R.drawable.card_7c, R.drawable.card_7d, R.drawable.card_7h, R.drawable.card_7s, R.drawable.card_8c, R.drawable.card_8d, R.drawable.card_8h, R.drawable.card_8s,
+                                            R.drawable.card_9c, R.drawable.card_9d, R.drawable.card_9h, R.drawable.card_9s, R.drawable.card_10c, R.drawable.card_10d, R.drawable.card_10h, R.drawable.card_10s,
+                                            R.drawable.card_11c,R.drawable.card_11d, R.drawable.card_11h, R.drawable.card_11s, R.drawable.card_12c, R.drawable.card_12d, R.drawable.card_12h, R.drawable.card_12s,
+                                            R.drawable.card_13c, R.drawable.card_13d, R.drawable.card_13h, R.drawable.card_13s};
+
     // Alerts
     AlertDialog.Builder playAgain;
 
@@ -50,18 +39,11 @@ public class MainActivity extends AppCompatActivity
     private TextView pickCardText;
 
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
 
         //Card Buttons
         ImageButton cardOne = (ImageButton) findViewById(R.id.card1);
@@ -128,8 +110,6 @@ public class MainActivity extends AppCompatActivity
         cardSixteenLsn cSixteen = new cardSixteenLsn();
         cardSixteen.setOnClickListener(cSixteen);
     }
-
-
 
 
 
@@ -251,7 +231,7 @@ public class MainActivity extends AppCompatActivity
     {
         for (int i = 0; i < numCards ; i++)
         {
-            cards[i] = 
+            
         }
     }
 }
